6e5fcfad31acff2126322266856001199051c3d7
V-Shashwath <shashwath2v4@gmail.com>
2025-07-21 16:58:26 +0530

    all

---
+++ src/main/java/p2p/App.java
@@ -0,0 +1,36 @@
+package p2p;
+
+import p2p.controller.FileController;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        try {
+            FileController fileController = new FileController(8080);
+            fileController.start();
+            System.out.println("PeerCast server started on port 8080");
+            System.out.println("UI available at http://localhost:3000");
+            Runtime.getRuntime().addShutdownHook(
+                    new Thread(
+                            () -> {
+                                System.out.println("Shutting down the server");
+                                fileController.stop();
+                            }
+                    )
+            );
+            System.out.println("Press Enter to stop the server");
+            System.in.read();
+
+        } catch (Exception ex) {
+            System.err.println("Failed to start the server at port: 8080 ");
+            ex.printStackTrace();
+        }
+
+
+    }
+}
